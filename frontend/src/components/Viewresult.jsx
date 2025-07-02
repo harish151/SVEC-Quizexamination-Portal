fa181a673d272170c7e1cb5f803f923073082566
Shankar1728 <neelapalashankar876@gmail.com>
2025-07-02 11:29:07 +0530

    commit

--- frontend/src/components/Viewresult.jsx
+++ frontend/src/components/Viewresult.jsx
@@ -71,7 +71,7 @@ function Viewresult() {
         setExam_type={setExam_type}
         sections={sections}
         setSections={setSections}
-        selectedsec = {"A"}
+        selectedsec = {selectedsec}
         setSelectedsec = {setSelectedsec}
         setDisplay={0}
         buttonname = {buttonname}
